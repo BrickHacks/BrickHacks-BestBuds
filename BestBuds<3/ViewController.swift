aba98af056fd5f521eb1104f1ee097491f117d02
adc2108 <adc2108@rit.edu>
2022-03-05 16:38:36 -0500

    Initial Commit

---
+++ BestBuds<3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestBuds<3
+//
+//  Created by Alexandra Ciesielka on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
